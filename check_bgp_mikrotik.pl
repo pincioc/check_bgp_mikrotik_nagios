de4015112b3ea20aaf418fe209e56099b5acadcf
Mauro <mauro@openskills.it>
2016-07-29 15:54:18 +0200

    Add files via upload
    the first time

---
+++ check_bgp_mikrotik.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl -w 
+# nagios:-epn
+#
+# check_bgp_mikrotik - nagios plugin
+#
+# Copyright (C) 2014 Mauro Fiore
+#
+#use strict;
+use lib qw( /usr/lib/nagios/plugins );
+use utils qw(%ERRORS $TIMEOUT &print_revision &support &usage);
+use Switch;
+use Mtik;
+use Nagios::Plugin::Getopt;
+$ng = Nagios::Plugin::Getopt->new(
+	usage => 'Usage: %s -H mtik_host -u mtik_user -p mtik_passwd -b BGP_peer_ip',
+	version => '0.1 [http://www.openskills.it]',
+);
+$ng->arg(spec => 'host|H=s', help => "Mikrotik Host", required => 1);
+$ng->arg(spec => 'user|u=s', help => "API username", required => 1);
+$ng->arg(spec => 'pass|p=s', help => "API password", required => 1);
+$ng->arg(spec => 'bgppeer|b=s', help => "BGP Peer IP", required => 1);
+$ng->getopts;
+$Mtik::debug = 0;
+if (Mtik::login($ng->get('host'),$ng->get('user'),$ng->get('pass')))
+	{
+	my @cmd = ("/routing/bgp/peer/print");
+	my($retval,@results) = Mtik::raw_talk(\@cmd);
+	$loo=0;
+	foreach my $result (@results) {
+		printf "$result\n";
+		my @values = split('=', $result);
+		$nums= @values;
+		if ($nums == 3){ 
+    			$chiave=$values[1];	
+			$valore=$values[2];
+			if ($chiave eq "remote-address"){
+				if ($valore eq $ng->get('bgppeer')){
+					$loo = 1;
+				}else {
+					$loo = 0;	
+				}
+			}
+			if ($loo == 1){
+				switch ($chiave){
+					case "state"	{  $status=$valore }
+					case "uptime"	{  $upfrom=$valore }
+				}
+			}
+  		}	
+	}
+	Mtik::logout;
+	if ($loo == 0){
+		print "UNKNOWN - Wrong remote peer address or peer disable \n"; 
+		exit (1);
+	}
+	if ($status eq "established"){
+		print "OK - Peer BGP $status from $upfrom\n"; 
+		exit (0);
+		
+	}else{
+		print "CRITICAL - State $status\n"; 
+		exit (2); 
+	}
+}else{
+		print "UNKNOWN - I can't log in to ($ng->get('host')\n"; 
+		exit (1); 
+}
